d7664221d1a16cc9c7b5911231e092290391f103
Oliver Galusinski <64497107+OliverGalusinski@users.noreply.github.com>
2023-05-18 16:30:01 +0200

    First commit, added a SVG background which rotates depending on the Screen Orientaition.
    Still doesnt work on IOS Tablets.

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import Constants from "expo-constants";
+import Svg, {Rect, Pattern, Defs} from "react-native-svg";
+import * as ScreenOrientation from 'expo-screen-orientation';
+import {useEffect, useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={{flex: 1, marginTop: Constants.statusBarHeight}}>
+            <BackgroundCard/>
+        </View>
+    );
 }
+const BackgroundCard = () => {
+    const [windowWidth, setWindowWidth] = useState(Dimensions.get('window').width);
+    const [windowHeight, setWindowHeight] = useState(Dimensions.get('window').height);
+
+    useEffect(() => {
+        Dimensions.addEventListener('change', ({window: {width, height}}) => {
+            setWindowWidth(width)
+            setWindowHeight(height)
+        });
+    });
+
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Svg height={windowHeight - Constants.statusBarHeight} width={windowWidth}>
+                <Defs>
+                    <Pattern
+                        id="pattern"
+                        patternUnits="userSpaceOnUse"
+                        width={windowWidth}
+                        height={windowHeight}
+                    >
+                        <Rect x="0" y="0" width={windowWidth} height={windowHeight} fill="blue"/>
+                    </Pattern>
+                </Defs>
+                <Rect
+                    x="5%"
+                    y="5%"
+                    width="90%"
+                    height="90%"
+                    rx={10}
+                    ry={10}
+                    fill="url(#pattern)"
+                />
+            </Svg>
+        </View>
+    );
+};
+
 
 const styles = StyleSheet.create({
   container: {
