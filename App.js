17c81c1ad1b3e948b81e507fd1094d283db177a8
Oliver Galusinski <64497107+OliverGalusinski@users.noreply.github.com>
2023-05-18 17:04:02 +0200

    - Removed dynamic Orientation + added first background Gradient
    Still doesnt work on Tablet have to add that to issues
    
    And have to change colours

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import Constants from "expo-constants";
-import Svg, {Rect, Pattern, Defs} from "react-native-svg";
+import Svg, {Rect, Pattern, Defs, LinearGradient, Stop, Path} from "react-native-svg";
 import * as ScreenOrientation from 'expo-screen-orientation';
 import {useEffect, useState} from "react";
 
 export default function App() {
     return (
-        <View style={{flex: 1, marginTop: Constants.statusBarHeight}}>
-            <BackgroundCard/>
+        <View style={{height: "100%", width: "100%", backgroundColor: "aqua"}}>
+            <View style={{flex: 1, marginTop: Constants.statusBarHeight+10}}>
+                <BackgroundCard/>
+            </View>
         </View>
     );
 }
@@ -16,34 +18,24 @@ const BackgroundCard = () => {
     const [windowWidth, setWindowWidth] = useState(Dimensions.get('window').width);
     const [windowHeight, setWindowHeight] = useState(Dimensions.get('window').height);
 
-    useEffect(() => {
-        Dimensions.addEventListener('change', ({window: {width, height}}) => {
-            setWindowWidth(width)
-            setWindowHeight(height)
-        });
-    });
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
 
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Svg height={windowHeight - Constants.statusBarHeight} width={windowWidth}>
                 <Defs>
-                    <Pattern
-                        id="pattern"
-                        patternUnits="userSpaceOnUse"
-                        width={windowWidth}
-                        height={windowHeight}
-                    >
-                        <Rect x="0" y="0" width={windowWidth} height={windowHeight} fill="blue"/>
-                    </Pattern>
+                    <LinearGradient id="path" x="0" y="0">
+                        <Stop offset="0" stopColor="#FFD080" stopOpacity="1" />
+                        <Stop offset="1" stopColor="red" stopOpacity="1" />
+                    </LinearGradient>
                 </Defs>
                 <Rect
                     x="5%"
-                    y="5%"
                     width="90%"
-                    height="90%"
+                    height="97%"
                     rx={10}
                     ry={10}
-                    fill="url(#pattern)"
+                    fill="url(#path)"
                 />
             </Svg>
         </View>
